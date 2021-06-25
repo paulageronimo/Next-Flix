a66c8d560dfd8796a7ca17f3c64bd9a3f8a3c200
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-06-25 05:14:30 -0400

    end of table view setup.
    on the lookout for the api key changes, might move a couple stuff so it will either make it or break it

--- Next-Flix/View Controllers/MoviesViewController.m
+++ Next-Flix/View Controllers/MoviesViewController.m
@@ -18,10 +18,10 @@
 @interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate> // implementing a protocal is a promise to find inside...
 // DataSource: two required components (rows/section and data path)
 // Delegate: no required
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies; // property, bc it is a thing... duh.... + type and name; getter and setter array
 // nonatomic, strong, defines the getter and setter
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
-
 @end
 
 @implementation MoviesViewController // warning
@@ -33,7 +33,7 @@
     self.tableView.dataSource = self; // setting it to the view controller
     self.tableView.delegate = self; // expecting it
     
-    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=1901d344fe219bef0495b3e27f38a318"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -42,8 +42,10 @@
             }
             else {
                 NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                
                 NSLog(@"%@", dataDictionary);
                 //NSArray *movies = dataDictionary[@"results"];
+                
                 self.movies = dataDictionary[@"results"]; // instead of saving it locally, it will be an instace property ready for table view
                 for (NSDictionary *movie in self.movies) {
                     NSLog(@"%@", movie[@"title"]); // @ @ @ @ @ @
@@ -53,12 +55,11 @@
                 [self.tableView reloadData]; // says "hey call the data source methods bc movies might have changed"
                 
                 // TODO: Get the array of movies
-                
                 // TODO: Store the movies in a property to use elsewhere
-                
                 // TODO: Reload your table view data
             }
         }];
+    [task resume]; // forgot to add this.. idk why but it fixed a bug.
 }
 
 -(void)didReceiveMemoryWarning {
