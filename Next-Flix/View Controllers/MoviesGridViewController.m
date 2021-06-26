f99c5706278eb1dc4cc4171afaeb62e79612e408
Paula Letícia Gerónimo <paulagero182@gmail.com>
2021-06-25 21:50:31 -0400

    displays movies in collection view, awkward white spacing in between

---
+++ Next-Flix/View Controllers/MoviesGridViewController.m
@@ -0,0 +1,91 @@
+//
+//  MoviesGridViewController.m
+//  Next-Flix
+//
+//  Created by Paula Leticia Geronimo on 6/25/21.
+//
+
+#import "MoviesGridViewController.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface MoviesGridViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (nonatomic, strong) NSArray *movies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@end
+
+@implementation MoviesGridViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+    
+    [self fetchMovies];
+}
+
+-(void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+-(void)fetchMovies {
+    //[self.activityIndicator startAnimating];
+
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=1901d344fe219bef0495b3e27f38a318"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+                // could show error view
+            }
+            else {
+                
+                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+                
+
+                self.movies = dataDictionary[@"results"];
+                [self.collectionView reloadData];
+
+            }
+
+        }];
+    [task resume];
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    
+    NSDictionary *movie = self.movies[indexPath.item];
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    // glue the above together :)
+    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+    cell.posterView.image = nil;
+    [cell.posterView setImageWithURL:posterURL];
+    
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+// for reference, I clicked 'fix', this below appeared. We are supposed to delete it and only keep the first two above :)
+// the first two are required, the rest are pretty much optional
+
+@end
---
+++ Next-Flix/Views/MovieCollectionCell.m
@@ -0,0 +1,12 @@
+//
+//  MovieCollectionCell.m
+//  Next-Flix
+//
+//  Created by Paula Leticia Geronimo on 6/25/21.
+//
+
+#import "MovieCollectionCell.h"
+
+@implementation MovieCollectionCell
+
+@end
